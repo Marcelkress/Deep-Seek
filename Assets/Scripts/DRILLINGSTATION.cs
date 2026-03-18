95fcc3eae5c2da0adb9af4f4900e0d6e79e0466a
Marcelkress <marcel@kress.dk>
2026-03-18 11:18:19 +0100

    l

--- Assets/Scripts/DRILLINGSTATION.cs
+++ Assets/Scripts/DRILLINGSTATION.cs
@@ -1,11 +1,21 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class DRILLINGSTATION : MonoBehaviour
 {
     // Fuh' yeh' 
-    
+    public enum Color
+    {
+        Red,
+        Green,
+        Blue,
+        Yellow
+    }
 
+    public List<Color> colorSequence;
+    public Light[] indicationLights;
+    
     void Start()
     {
         
