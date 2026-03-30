721b611833861e531f96a3a024de0cdef18a5db4
JacobVigsoe <eh66xs@student.aau.dk>
2026-03-30 11:40:43 +0200

    main level world building

--- Assets/Scripts/PlayerOxygen.cs
+++ Assets/Scripts/PlayerOxygen.cs
@@ -1,8 +1,12 @@
+using System.Collections;
 using Cinemachine;
 using StarterAssets;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Experimental.GlobalIllumination;
 using UnityEngine.InputSystem;
+using DG.Tweening;
 
 public class PlayerOxygen : MonoBehaviour
 {
@@ -26,6 +30,17 @@ public class PlayerOxygen : MonoBehaviour
     private int amountToReplenish;
     public UnityEvent ReplenishStart, ReplenishEnd, StartCoolDownEvent, ResetCooldownEvent;
 
+    public UnityEvent LowOxygenWarningEvent;
+
+    [Header("Low oxygen Warning")]
+    [SerializeField, Range(0f, 100f)] private float lowOxygenPercent = 10f; // 10 = 10%
+    [SerializeField] private Light lowOxygenLight; // blinking light to indicate low oxygen
+    [SerializeField] private float lightFadeDuration = 0.5f; // duration for fading the light in and out
+    [SerializeField] private float lowOxygenBlinkInterval = 0.5f; // interval for blinking light
+    public bool IsLowOxygen => currentOxygen <= LowOxygenThreshold;
+    public int LowOxygenThreshold => Mathf.CeilToInt(maxOxygen * (lowOxygenPercent / 100f));
+
+    private float initialLightIntensity;
     private bool walking, sprinting;
     
     void Awake()
@@ -35,6 +50,9 @@ public class PlayerOxygen : MonoBehaviour
         currentLoss = oxygenLossPrSWalk;
         replenishCooldownTimer = 30;
         virtualCamera = GetComponent<CinemachineVirtualCamera>();
+
+        initialLightIntensity = lowOxygenLight.intensity;
+        lowOxygenLight.intensity = 0; // Start with the light off
     }
     
     // Update is called once per frame
@@ -51,6 +69,46 @@ public class PlayerOxygen : MonoBehaviour
         ReplenishUpdate();
         
         OxygenLossUpdate();
+
+        if (!warningActive)
+        {
+            StartCoroutine(LowOxygenWarningCoroutine());
+        }
+
+        if (currentOxygen <= 0)
+        {
+            Die();
+        }
+    }
+
+    private bool warningActive = false;
+    private IEnumerator LowOxygenWarningCoroutine()
+    {
+        if (!IsLowOxygen)
+        {
+            warningActive = false;
+            lowOxygenLight.DOIntensity(0, lightFadeDuration); // Smooth fade out if it shouldn't be blinking
+            yield break;
+        }
+
+        warningActive = true;
+
+        while (IsLowOxygen)
+        {
+            LowOxygenWarningEvent.Invoke();
+            
+            // Smoothly fade to full intensity over the whole interval
+            lowOxygenLight.DOIntensity(initialLightIntensity, lowOxygenBlinkInterval).SetEase(Ease.InOutSine);
+            yield return new WaitForSeconds(lowOxygenBlinkInterval); 
+            
+            // Smoothly fade back down to 0 over the whole interval
+            lowOxygenLight.DOIntensity(0, lowOxygenBlinkInterval).SetEase(Ease.InOutSine);
+            yield return new WaitForSeconds(lowOxygenBlinkInterval);
+        }
+
+        // Clean up once the loop ends (oxygen was replenished)
+        lowOxygenLight.DOIntensity(0, lightFadeDuration);
+        warningActive = false;
     }
 
     private void OxygenLossUpdate()
@@ -175,5 +233,11 @@ public class PlayerOxygen : MonoBehaviour
         currentOxygen -= amount;
         
         GetComponent<CinemachineImpulseSource>().GenerateImpulse();
+    }
+
+    private void Die()
+    {
+        Debug.Log("Player has died");
+        // Implement death logic here (e.g., respawn, game over screen, etc.)
     }
 }
